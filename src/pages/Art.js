2c58e122340df81758d932fe6b6e0bb29cc1d87d
mel <mellee115@hotmail.com>
2022-09-20 01:46:36 -0400

    Adding more images for the Art and Photos pages plus small SCSS file cleanup

--- src/pages/Art.js
+++ src/pages/Art.js
@@ -2,11 +2,14 @@ import React from "react";
 import {ImgGrid} from "../components/ImgGrid";
 
 import img1 from "../assets/images/art/blue gouache rose 1.png"
-import img2 from "../assets/images/art/abstract acrylic painting 2.jpg"
-import img3 from "../assets/images/art/bun coloured w mint green.png"
-import img4 from "../assets/images/art/tulips with blue grid.jpg"
-import img5 from "../assets/images/art/polymer clay crane.jpg"
-import img6 from "../assets/images/art/abstract acrylic painting 1.jpg"
+import img2 from "../assets/images/art/mushrooms stamp.jpg"
+import img3 from "../assets/images/art/mushrooms stamp print.jpg"
+import img4 from "../assets/images/art/abstract acrylic painting 2.jpg"
+import img5 from "../assets/images/art/bun coloured w mint green.png"
+import img6 from "../assets/images/art/tulips with blue grid.jpg"
+import img7 from "../assets/images/art/gouache pear.jpg"
+import img8 from "../assets/images/art/polymer clay crane.jpg"
+import img9 from "../assets/images/art/abstract acrylic painting 1.jpg"
 
 export const Art = () => {
   //TODO: rewrite this in a better way?
@@ -17,6 +20,9 @@ export const Art = () => {
     {src:img4},
     {src:img5},
     {src:img6},
+    {src:img7},
+    {src:img8},
+    {src:img9},
   ]
 
   return(<>
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,15 +1,18 @@
 import React from "react";
 import {ImgGrid} from "../components/ImgGrid";
 
-import img1 from "../assets/images/photos/Sweet Dreams Noodle Soup Special.JPG"
-import img2 from "../assets/images/photos/Sweet Dreams Noodle Soup Smile.JPG"
-import img3 from "../assets/images/photos/Sweet Dreams Mango Bingsu Special.JPG"
-import img4 from "../assets/images/photos/Sweet Dreams Overhead Noodle Soup Special.JPG"
-import img5 from "../assets/images/photos/Macarons.JPG"
-import img6 from "../assets/images/photos/Sweet Dreams Hot Drink Special.JPG"
-import img7 from "../assets/images/photos/Sweet Dreams at night.JPG"
-import img8 from "../assets/images/photos/Super Flower Blood Moon.JPG"
+import img1 from "../assets/images/photos/sakura.JPG"
+import img2 from "../assets/images/photos/Sweet Dreams Noodle Soup Special.JPG"
+import img3 from "../assets/images/photos/Sweet Dreams Noodle Soup Smile.JPG"
+import img4 from "../assets/images/photos/Sweet Dreams Mango Bingsu Special.JPG"
+import img5 from "../assets/images/photos/Sweet Dreams Overhead Noodle Soup Special.JPG"
+import img6 from "../assets/images/photos/Macarons.JPG"
+import img7 from "../assets/images/photos/Sweet Dreams Hot Drink Special.JPG"
+import img8 from "../assets/images/photos/Sweet Dreams at night.JPG"
 import img9 from "../assets/images/photos/Rock Dunder Tiny Mushrooms.JPG"
+import img10 from "../assets/images/photos/Rock Dunder Tree Mushrooms.JPG"
+import img11 from "../assets/images/photos/IMG_1737.JPG"
+import img12 from "../assets/images/photos/Super Flower Blood Moon.JPG"
 
 export const Photos = () => {
   //TODO: rewrite this in a better way?
@@ -23,6 +26,9 @@ export const Photos = () => {
    {src:img7},
    {src:img8},
    {src:img9},
+   {src:img10},
+   {src:img11},
+   {src:img12},
  ]
 
   return(<>
