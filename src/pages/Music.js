15e6f98d3ff68baebf5fbee1f7f8f1bf1d0c4d3b
mel <mellee115@hotmail.com>
2022-09-20 04:08:08 -0400

    Shrinking and growing blob component to add to the background of the Music page

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,25 +1,30 @@
 import React from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faYoutube,faSpotify,faLastfm} from "@fortawesome/free-brands-svg-icons";
+import {faYoutube, faSpotify, faLastfm} from "@fortawesome/free-brands-svg-icons";
+import {Blob} from "../components/Blob";
 
 export const Music = () => {
-  return(
-    <ul>
-      <li>
-        <a href='https://www.last.fm/user/mellee115'>
-          <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faLastfm}/> Last.fm Listening Charts
-        </a>
-      </li>
-      <li>
-        <a href='https://open.spotify.com/user/22gkfywktzsjjowaa3fevoxoi?si=6057502a0d0b40db'>
-          <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faSpotify}/> Spotify Playlists
-        </a>
-      </li>
-      <li>
-        <a href='https://www.youtube.com/user/melissashawmusic/videos'>
-          <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faYoutube}/> YouTube Music Channel - melissashawmusic
-        </a>
-      </li>
-    </ul>
+  return (
+    <>
+      <ul>
+        <li>
+          <a href='https://www.last.fm/user/mellee115'>
+            <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faLastfm}/> Last.fm Listening Charts
+          </a>
+        </li>
+        <li>
+          <a href='https://open.spotify.com/user/22gkfywktzsjjowaa3fevoxoi?si=6057502a0d0b40db'>
+            <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faSpotify}/> Spotify Playlists
+          </a>
+        </li>
+        <li>
+          <a href='https://www.youtube.com/user/melissashawmusic/videos'>
+            <FontAwesomeIcon className='fa-2x m-r-10 svg-inline--fa' icon={faYoutube}/> YouTube Music Channel -
+            melissashawmusic
+          </a>
+        </li>
+      </ul>
+      <Blob />
+    </>
   )
 }
